b82a9d76c5a81d83afc107e393653e185eceda4d
LLapuras <chenwanwan13@gmail.com>
2020-01-28 11:25:11 +0000

    update

---
+++ LapuGolf/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//1.click object and add force
+//2. hold mouse add powerup force
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                   rb.AddForce(transform.forward * thrust*100);
+                }
+            }
+        }
+          
+    }
+
+}
---
+++ LapuGolf/Assets/Scripts/test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+  void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider);
+                }
+            }
+        }
+    }
+}
